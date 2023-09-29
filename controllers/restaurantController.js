d78bd505d48da432aba496a3ea1b918e095f19bb
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-09-28 22:07:26 -0600

    api-restaurants by cuisine completed

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -4,7 +4,7 @@ const {Restaurant} = require("../models/restaurant");
 const getAllRestaurants = async (req, res) => {
   try {
     // You can filter restaurants based on the area (location) here
-    const restaurants = await Restaurant.find({});
+    const restaurants = await Restaurant.find();
     res.json(restaurants);
   } catch (error) {
     console.error(error);
@@ -12,6 +12,18 @@ const getAllRestaurants = async (req, res) => {
   }
 };
 
+const getRestaurantsByCuisine = async(req,res)=>{
+    try{
+        const byCuisine = await Restaurant.find({cuisine:req.body.cuisine})
+        res.json(byCuisine);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
 module.exports = {
   getAllRestaurants,
+  getRestaurantsByCuisine
 };
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getAllRestaurants } = require("../controllers/restaurantController");
+const { getAllRestaurants,get, getRestaurantsByCuisine } = require("../controllers/restaurantController");
 const protect = require("../middleware/authMiddleware");
 
 // Route for user registration
 router.get("/getAllRestaurants", getAllRestaurants);
+router.post("/getRestaurantsByCuisine",getRestaurantsByCuisine);
 
 module.exports = router;
