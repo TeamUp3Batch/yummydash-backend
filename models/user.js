48f2d048cf3e754d769604e58a2b05bcec04351c
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-10-13 14:11:13 -0600

    resolved conflicts

--- models/user.js
+++ models/user.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-  firstName: {type: String, required: true},
-  lastName: {type: String, required: true},
-  email: {type: String, required: true},
-  password: {type: String, required: true},
-  phoneNumber: {type: String, required: false},
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: String, required: false },
   address: [
     {
       unitNumber: String,
