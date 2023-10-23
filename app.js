97b9eeb1912f4423b0030b44b6d1f20fa9495380
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-10-22 20:50:45 -0600

    modified add to cart function

--- app.js
+++ app.js
@@ -1,49 +1,50 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
-const usersRoutes = require('./routes/usersRoutes');
-const authRoutes = require('./routes/authRoutes');
-const restaurantRoutes = require('./routes/restaurantRoutes');
-const cuisineRoutes = require('./routes/cuisineRoutes');
-const cartRoutes = require('./routes/cartRoutes');
-const logger = require('./utils/logger');
-const session = require('express-session');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const connectDB = require('./config/db')
+const usersRoutes = require('./routes/usersRoutes')
+const authRoutes = require('./routes/authRoutes')
+const restaurantRoutes = require('./routes/restaurantRoutes')
+const cuisineRoutes = require('./routes/cuisineRoutes')
+const cartRoutes = require('./routes/cartRoutes')
+const logger = require('./utils/logger')
+const session = require('express-session')
 
 // database connection
 
-connectDB();
+connectDB()
 
 // middlewares
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 app.use(
     session({
-      secret: process.env.SESSION_SECRET,
-      cookie: {
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000,
-      },
-    }),
-);
+        secret: process.env.SESSION_SECRET,
+        cookie: {
+            secure: false,
+            maxAge: 24 * 60 * 60 * 1000,
+        },
+        resave: true,
+        saveUninitialized: true,
+    })
+)
 
 // routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/restaurants', restaurantRoutes);
-app.use('/api/cuisines', cuisineRoutes);
-app.use('/api/cart', cartRoutes);
-
+app.use('/api/auth', authRoutes)
+app.use('/api/users', usersRoutes)
+app.use('/api/restaurants', restaurantRoutes)
+app.use('/api/cuisines', cuisineRoutes)
+app.use('/api/cart', cartRoutes)
 
 // testing route
 app.get('/test', (req, res) => {
-  res.send('Hello');
-});
-const PORT = process.env.PORT || 5000;
+    res.send('Hello')
+})
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
-});
+    logger.info(`Server is running on port ${PORT}`)
+})
--- controllers/cartController.js
+++ controllers/cartController.js
@@ -7,30 +7,11 @@ const addToCart = async (req, res) => {
         const userId = req.body.userId
         const menuId = req.body.menuId
         const cartId = req.body.cartId
-        const quantity = parseInt(req.body.quantity) || 1 // Default to 1 if not provided
+        const quantity = parseInt(req.body.quantity) || 1
         const restaurant = await Restaurant.findById(restaurantId)
 
         if (!restaurant) {
             return res.status(404).json({ message: 'Restaurant not found' })
-        }
-
-        // Find or create a cart based on cartId
-        let cart
-
-        if (cartId) {
-            cart = await Cart.findById(cartId)
-
-            if (!cart) {
-                return res.status(404).json({ message: 'Cart not found' })
-            }
-        } else {
-            // Create a new Cart
-            cart = new Cart({
-                userId,
-                restaurantId,
-                menuItems: [],
-                total: 0,
-            })
         }
 
         // Find the menu item to add to the cart
@@ -42,32 +23,60 @@ const addToCart = async (req, res) => {
             })
         }
 
-        // Check if the item already exists in the cart
-        const existingCartItem = cart.menuItems.id(menuId)
-        console.log("existing cart item",existingCartItem)
-        if (existingCartItem) {
-            // If it exists, update the quantity and total price
-            existingCartItem.quantity += quantity
-            existingCartItem.price += quantity * menuItem.price
-        } else {
-            // If it doesn't exist, add it to the cart
-            cart.menuItems.push({
-                itemId: menuId,
-                name: menuItem.name,
-                perPrice: menuItem.price,
-                price: menuItem.price * quantity,
-                quantity,
-            })
-            console.log("new cart",cart)
-        }
+        // Find or create a cart based on cartId
+        let cart
 
-        // Update the cart's total price
-        cart.total += menuItem.price * quantity
+        if (cartId) {
+            cart = await Cart.findById(cartId)
+
+            if (!cart) {
+                return res.status(404).json({ message: 'Cart not found' })
+            }
+
+            // Check if the item already exists in the cart
+            const existingCartItem = cart.menuItems.find(
+                (item) => item.itemId === menuId
+            )
+            if (existingCartItem) {
+                existingCartItem.quantity += quantity
+                existingCartItem.price =
+                    existingCartItem.quantity * menuItem.price
+                cart.total += quantity * menuItem.price
+                await cart.save()
+            } else {
+                // If it doesn't exist, add it to the cart
+                cart.menuItems.push({
+                    itemId: menuId,
+                    name: menuItem.name,
+                    perPrice: menuItem.price,
+                    price: menuItem.price * quantity,
+                    quantity,
+                })
+                cart.total += quantity * menuItem.price
+                await cart.save()
+            }
+        } else {
+            // Create a new Cart
+            cart = new Cart({
+                userId,
+                restaurantId,
+                menuItems: [
+                    {
+                        itemId: menuId,
+                        name: menuItem.name,
+                        perPrice: menuItem.price,
+                        price: menuItem.price * quantity,
+                        quantity,
+                    },
+                ],
+                total: menuItem.price * quantity,
+            })
+        }
 
         // Save or update the cart in the database
         await cart.save()
 
-        res.status(201).json({cart})
+        res.status(201).json({ cart })
     } catch (error) {
         console.error(error)
         res.status(500).json({ message: 'Internal Server Error' })
