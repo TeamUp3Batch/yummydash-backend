6bf2286292558da439978ab2495e46feac1d94b9
anithaamarnath <anithaamarnath04@gmail.com>
2023-10-24 17:22:41 -0700

    Added checkout details API

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,27 +1,51 @@
-// cart- id cart detials -> 
-//find the estimate formt the restaurant 
-//using the (req and body )restaurant id ,cart id
-//
 
+const {Cart} = require('../models/cart');
+const {Restaurant} = require('../models/restaurant');
 
 const getCartDetailsToCheckout = async (req, res) => {
   try {
     const cartId = req.body.cartId;
     const restaurantId = req.body.restaurantId;
-    console.log('cartId', cartId);
-    console.log('restaurantId', restaurantId);
+    const userId = req.body.userId;
 
-    // if (!restaurantId) {
-    //   return res.status(404).json({message: 'Restaurant not found'});
-    // }
+    if (!restaurantId) {
+      return res.status(400).json({error: 'restaurantId is required'});
+    }
 
-    // if (!cartId) {
-    //   return res.status(404).json({message: 'Cart not found'});
-    // }
+    if (!userId) {
+      return res.status(400).json({error: 'UserId is required'});
+    }
 
-    res.status(200).json({message: 'success'});
+    if (!cartId) {
+      return res.status(400).json({error: 'cartId is required'});
+    }
+
+    const cart = await Cart.findById(cartId);
+
+    if (!cart) {
+      return res.status(400).json({error: ' cart not found '});
+    }
+
+    const restaurant = await Restaurant.findById(restaurantId);
+
+    if (!restaurant) {
+      return res.status(400).json({error: 'restaurant not found '});
+    }
+
+    const checkoutDetails = {
+      cartId: cart._id,
+      restaurantId: restaurant._id,
+      userId: userId,
+      restaurantName: restaurant.name,
+      totalprice: cart.price,
+      lineItems: cart.menuItems,
+      estimatedTime: restaurant.estimatedDeliveryTime.minEstimatedTime,
+      paymentType: 'CREDIT_CARD', // once map BOX is done we can add address
+    };
+
+    res.status(201).json(checkoutDetails);
   } catch (error) {
-    console.error('Error in getCartDetails:', error);
+    console.error('Error in getCartDetailsToCheckout:', error);
     res.status(500).json({message: 'Internal Server Error'});
   }
 };
