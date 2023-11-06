473a4ec9b9ab790ae308767c00b60e1021b56651
Kavitha Shanmugan <kavitha.shanmugan@gmail.com>
2023-11-06 16:00:47 -0600

    added deletecart function

--- models/cart.js
+++ models/cart.js
@@ -1,23 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const cartSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    restaurantId: { type: String, required: true },
+    menuItems: [
+        {
+            itemId: { type: String },
+            name: { type: String },
+            perPrice: { type: Number },
+            price: { type: Number },
+            quantity: { type: Number },
+        },
+    ],
+    total: { type: Number, required: true, default: 0 },
+    cartCreationTime: { type: Date, default: Date.now },
+    checkoutTime: { type: Date },
+    country: { type: String },
+    currency: { type: String },
+    paymentType: { type: String },
+    orderStatus: {
+        type: String,
+        enum: [
+            'initial',
+            'payment',
+            'acceptance',
+            'preparation',
+            'pickup',
+            'delivery',
+        ],
+        default: 'initial',
+    },
+})
 
-  userId: {type: String, required: true},
-  restaurantId: {type: String, required: true},
-  menuItems: [{
-    itemId: {type: String},
-    name: {type: String},
-    perPrice: {type: Number},
-    price: {type: Number},
-    quantity: {type: Number}}],
-  total: {type: Number, required: true, default: 0},
-  cartCreationTime: {type: Date, default: Date.now},
-  checkoutTime: {type: Date},
-  country: {type: String},
-  currency: {type: String},
-  paymentType: {type: String},
-});
+const Cart = mongoose.model('cart', cartSchema)
 
-const Cart = mongoose.model('cart', cartSchema);
-
-module.exports = {Cart};
+module.exports = { Cart }
