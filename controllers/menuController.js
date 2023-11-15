5cdd143532c6ea5a2c702102cd68adfd31b7963c
Anitha Amarnath <41219364+anithaamarnath@users.noreply.github.com>
2023-11-14 17:28:31 -0800

    Update menuController.js

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -98,8 +98,7 @@ const addMenuItemToRestaurant = async (req, res) => {
           return res.status(404).json({ message: 'Menu item not found' });
         }
 
-        const restaurant = await Restaurant.findById(restaurantId);
-        const menuItems = restaurant.menu;
+      
         //return rest of menuItems
         return res.status(200).json({ message: 'Menu item deleted successfully', status: 'success' , menu : result.menu});
 
